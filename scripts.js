e1cdb4f5779e2d5cd4f2c1c7c918ee9ce60e13fd
monodram <dwiktorek11@gmail.com>
2022-06-08 10:48:59 +0200

    Added randomly generated computer selection

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+let rockPaperScissors = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+    return Math.floor(Math.random() * rockPaperScissors.length);
+};
+
+console.log(computerPlay(rockPaperScissors));
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
