3bb0cc0136ee1464ae68d5d1132d169f81949236
monodram <dwiktorek11@gmail.com>
2022-06-11 17:04:45 +0200

    Finished

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-
-let rockPaperScissors = ['rock', 'paper', 'scissors'];
-
 // randomly generates computer choice
+let rockPaperScissors = ['rock', 'paper', 'scissors'];
 function computerPlay(rockPaperScissors) {
     return Math.floor(Math.random() * rockPaperScissors.length);
 };
@@ -11,7 +9,7 @@ let computerScore = 0;
 let whoWinsRound = 0;
 let computerSelection = computerPlay(rockPaperScissors);
 
-// decides who wins a single round
+// decides who wins a single round and gives +1 point to the winner
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection == 'rock') && (computerSelection == 1)) {
         return computerScore++,
@@ -45,38 +43,29 @@ function playRound(playerSelection, computerSelection) {
 // window alert that shows who won a single round
 function roundScore() {
     if (whoWinsRound == 2) {
-        alert('You won this round.');
+        alert(`You won this round. 
+        Your score: ${playerScore} Computer score: ${computerScore}`);
+
     } else if (whoWinsRound == 1) {
-        alert('You lost this round.');
+        alert(`You lost this round.
+        Your score: ${playerScore} Computer score: ${computerScore}`);
+
     } else {
         alert('It\'s a tie. Try again.');
     }
 };
 
-// plays round 5 times
-for (i = 0; i < 5; i++) {
+// loops the game until someone wins 3 rounds
+for (let i = 0; i < 15; i++) {
     let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
     let roundResult = playRound(playerSelection, computerSelection);
     roundScore(roundResult);
+    if (playerScore == 3) {
+        alert('You\'ve won! Congratulations :)');
+        break;
+
+    } else if (computerScore == 3) {
+        alert('You\'ve lost :( Better luck next time.');
+        break;
+    }
 };
-
-
-
-function gameScore() {
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
